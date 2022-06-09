2404f89931dc6bf930e68dd5079433e61ab6774c
afdhal.yusman <afdhal.yusman22@gmail.com>
2022-06-09 18:50:11 +0700

    -

--- api/farm.js
+++ api/farm.js
@@ -39,47 +39,47 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.post('/', async (req, res) => {
-    try {
-        let roleId = req.body.role_id;
-        let menus = req.body.menu_id;
-        const cekExist = await RoleMenu.findOne({
-            attributes: ['role_id', 'menu_id'],
-            where: {
-                [Op.and]: [{
-                    role_id: roleId
-                }, {
-                    menu_id: menus
-                }]
-            }
-        });
+// router.post('/', async (req, res) => {
+//     try {
+//         let roleId = req.body.role_id;
+//         let menus = req.body.menu_id;
+//         const cekExist = await RoleMenu.findOne({
+//             attributes: ['role_id', 'menu_id'],
+//             where: {
+//                 [Op.and]: [{
+//                     role_id: roleId
+//                 }, {
+//                     menu_id: menus
+//                 }]
+//             }
+//         });
         
-        if (cekExist != null) {
-            return res.status(400).send({
-                message: "mapping role-menu already exist"
-            })
-        }
+//         if (cekExist != null) {
+//             return res.status(400).send({
+//                 message: "mapping role-menu already exist"
+//             })
+//         }
 
-        let insert_data = RoleMenu.create({
-            role_id: roleId,
-            menu_id: menus,
-        });
+//         let insert_data = RoleMenu.create({
+//             role_id: roleId,
+//             menu_id: menus,
+//         });
 
-        if (!insert_data) {
-            return res.status(400).send({
-                message: "Error in insert new record"
-            })
-        }
+//         if (!insert_data) {
+//             return res.status(400).send({
+//                 message: "Error in insert new record"
+//             })
+//         }
 
-        return res.send({
-            message: 'Created'
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(400).send({
-            message: "Error"
-        })
-    }
-});
+//         return res.send({
+//             message: 'Created'
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(400).send({
+//             message: "Error"
+//         })
+//     }
+// });
 
 module.exports = router;
